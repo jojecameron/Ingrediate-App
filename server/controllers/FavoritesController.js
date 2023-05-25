eee648bf6db6797cf365514dd62c93141571aba5
jojecameron <jojecameron@gmail.com>
2023-05-25 15:05:31 -0700

    9th commit, new controllers and models

---
+++ server/controllers/FavoritesController.js
@@ -0,0 +1,44 @@
+const Favorites = require('../models/favoritesModel');
+
+
+const FavoritesController = {};
+
+// need to implement user authentication for this?
+FavoritesController.getFavorites = async (req, res, next) => {
+  const { username } = req.body;
+  try {
+    const favorites = await Favorites.find({});
+    res.locals.favorites = favorites;
+    return next();
+  } catch (err) {
+    return next({ err: err });
+  }
+};
+
+//adding favorite to database
+FavoritesController.addFavorite = async (req, res, next) => {
+  console.log('WE ARE IN ADDFAVORITES');
+  const { index, recipe } = req.body;
+  try {
+    const newFavorite = await Favorites.create({ favorites: [{ index, recipe }] });
+    res.locals.newFavorite = newFavorite;
+    return next();
+  } catch (err) {
+    return next({err: err});
+  }
+}
+
+//deleting favorite from database
+FavoritesController.deleteFavorite =  async (req, res, next) => {
+ console.log('WE ARE IN DELETEFAVORITES');
+  const { index } = req.body;
+  try {
+    const deletedFavorite = await Favorites.findOneAndDelete({ 'favorites.index': index });
+    res.locals.deletedFavorite = deletedFavorite;
+  } catch (err) {
+    return next({err: err});
+  }
+}
+
+
+module.exports = FavoritesController;
---
+++ server/models/favoritesModel.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+
+const favoritesSchema = new Schema({
+    favorites: [
+      {
+        index: { type: Number },
+        recipe: { type: Object }
+      }
+    ]
+});
+
+
+module.exports = mongoose.model('Favorite', favoritesSchema);
