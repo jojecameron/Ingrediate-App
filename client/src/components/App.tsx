ff9452787bd772867b639a4a1c8a4bef4b64a41d
jojecameron <jojecameron@gmail.com>
2023-11-02 13:30:14 -0700

    finished conversion of App to ts

---
+++ client/src/components/App.tsx
@@ -0,0 +1,11 @@
+import MainContainer from "../containers/MainContainer";
+
+const App = (): JSX.Element  => {
+  return (
+    <div className="App">
+      <MainContainer />
+    </div>
+  );
+}
+
+export default App;
