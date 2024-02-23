e33ce9e57fa3b9fa5f3cb0def83c27cc97765bc6
jojecameron <jojecameron@gmail.com>
2024-02-23 15:44:21 -0800

    added jest/husky, precommit coverage report, 1 test

---
+++ jest.config.ts
@@ -0,0 +1,21 @@
+module.exports = {
+  moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+  testMatch: [
+    '**/__tests__/**/*.+(ts|tsx|js|jsx)',
+    '**/?(*.)+(spec|test).+(ts|tsx|js|jsx)',
+  ],
+  testEnvironment: 'jsdom',
+  coverageDirectory: 'coverage',
+  collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}', '!src/**/*.d.ts'],
+  coverageThreshold: {
+    global: {
+      branches: 0,
+      functions: 0,
+      lines: 0,
+      statements: 0,
+    },
+  },
+};
