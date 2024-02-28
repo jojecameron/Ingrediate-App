0df54e284ebe7c15fa4eb1d9b2219580b9435dab
jojecameron <jojecameron@gmail.com>
2024-02-27 16:02:33 -0800

    refactor: add src folder to root, migrate server and client paths to src

--- __tests__/src/client/containers/RecipeContainer.test.tsx
+++ __tests__/src/client/containers/RecipeContainer.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import RecipeContainer from '../../../../client/src/containers/RecipeContainer/RecipeContainer';
+import RecipeContainer from '../../../../src/client/containers/RecipeContainer/RecipeContainer';
 
 const mockProps = {
   recipeList: [],
