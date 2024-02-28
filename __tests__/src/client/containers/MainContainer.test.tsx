32226151feb28998a5f14feefde4e02766a2f1f0
jojecameron <jojecameron@gmail.com>
2024-02-27 16:02:33 -0800

    refactor: add src folder to root, migrate server and client paths to src

--- __tests__/src/client/containers/MainContainer.test.tsx
+++ __tests__/src/client/containers/MainContainer.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from '@testing-library/react'; 
-import MainContainer from '../../../../client/src/containers/MainContainer/MainContainer';
+import MainContainer from '../../../../src/client/containers/MainContainer/MainContainer';
 
 test('renders a basic snapshot', () => {
   const { container } = render(<MainContainer />);
--- jest.config.ts
+++ jest.config.ts
@@ -2,20 +2,24 @@ module.exports = {
   moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
   transform: {
     '^.+\\.(ts|tsx)$': 'ts-jest',
+    '^.+\\.jsx?$': 'babel-jest',
   },
   testMatch: [
     '**/__tests__/**/*.+(ts|tsx|js|jsx)',
     '**/?(*.)+(spec|test).+(ts|tsx|js|jsx)',
   ],
+  randomize: true,
+  roots: ['<rootDir>/src/', '<rootDir>/__tests__/'],
   testEnvironment: 'jsdom',
   coverageDirectory: 'coverage',
   collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}', '!src/**/*.d.ts'],
+  coverageReporters: ['text', 'html'],
   coverageThreshold: {
     global: {
-      branches: 0,
-      functions: 0,
-      lines: 0,
-      statements: 0,
+      branches: 5,
+      functions: 5,
+      lines: 5,
+      statements: 5,
     },
   },
 };
